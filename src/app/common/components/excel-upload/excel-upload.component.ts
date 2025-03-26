f24ea237234c79addac8af2ff7e61631dd1c2455
Chau Hoang Hieu Lam <lamchauhoanghieu@gmail.com>
2025-03-26 13:05:11 +1100

    feat: Add ngx-spinner and show the spinner when uploading a new file

--- src/app/common/components/excel-upload/excel-upload.component.ts
+++ src/app/common/components/excel-upload/excel-upload.component.ts
@@ -1,6 +1,7 @@
-import { Component, output, viewChild } from '@angular/core';
+import { Component, inject, output, viewChild } from '@angular/core';
 import { ExcelData } from '../../models/file-upload.model';
 import * as XLSX from 'xlsx';
+import { NgxSpinnerService } from 'ngx-spinner';
 
 @Component({
   selector: 'app-excel-upload',
@@ -11,6 +12,7 @@ import * as XLSX from 'xlsx';
 export class ExcelUploadComponent {
   fileInput = viewChild('fileInput');
   fileSubmission = output<ExcelData>();
+  spinner: NgxSpinnerService = inject(NgxSpinnerService);
 
   /**
    * Accept excel or csv files only
@@ -26,26 +28,31 @@ export class ExcelUploadComponent {
     if (target.files.length !== 1) {
       throw new Error('Cannot use multiple files');
     }
+    this.spinner.show();
     const reader: FileReader = new FileReader();
-    reader.onload = (e: any) => {
-      // Read workbook
-      const bstr: string = e.target.result;
-      const wb: XLSX.WorkBook = XLSX.read(bstr, {
-        type: 'binary',
-      });
-      // Grab the first sheet
-      const wsname: string = wb.SheetNames[0];
-      const ws: XLSX.WorkSheet = wb.Sheets[wsname];
-      // Extract data and headers
-      const data = XLSX.utils.sheet_to_json(ws, {
-        header: 1,
-        raw: false,
-      }) as any;
-      const dataHeaders = data.shift();
-      const fileName = target.files[0].name;
-      // Emit data to output
-      this.fileSubmission.emit({ data, dataHeaders, fileName });
-    };
-    reader.readAsBinaryString(target.files[0]);
+
+    setTimeout(() => {
+      reader.onload = (e: any) => {
+        // Read workbook
+        const bstr: string = e.target.result;
+        const wb: XLSX.WorkBook = XLSX.read(bstr, {
+          type: 'binary',
+        });
+        // Grab the first sheet
+        const wsname: string = wb.SheetNames[0];
+        const ws: XLSX.WorkSheet = wb.Sheets[wsname];
+        // Extract data and headers
+        const data = XLSX.utils.sheet_to_json(ws, {
+          header: 1,
+          raw: false,
+        }) as any;
+        const dataHeaders = data.shift();
+        const fileName = target.files[0].name;
+        // Emit data to output
+        this.fileSubmission.emit({ data, dataHeaders, fileName });
+        this.spinner.hide();
+      };
+      reader.readAsBinaryString(target.files[0]);
+    }, 200);
   }
 }
